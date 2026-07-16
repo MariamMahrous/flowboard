3a7d155983974c134031f5f85e6a574314d72f3a
MariamMahrous <mariammahrous80@yahoo.com>
2026-07-16 03:13:37 +0300

    Implement authentication with Laravel Sanctum

---
+++ flowboard-api/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+
+class AuthController extends Controller
+{
+    
+public function register(RegisterRequest $request){
+    $user=User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>$request->password,
+   
+    ]);
+    return response()->json([
+        'message'=>'User registered successfully',
+        'user'=>$user
+    ],201);
+    
+
+}
+
+
+ public function login(LoginRequest $request){
+    
+    if(!Auth::attempt($request->only('email','password'))){
+        return response()->json(['message'=>'Invalid Credentials'],401);
+    }
+   
+    $user=User::where('email',$request->email)->firstOrfail();
+    $token=$user->createToken('auth_token')->plainTextToken;
+      return response()->json([
+        'message'=>'Login Successful',
+        'user'=>$user,
+        'token'=>$token
+    ],200);
+
+  }
+   
+  public function logout(Request $request){
+    $request->user()->currentAccessToken()->delete();
+     return response()->json([
+        'message'=>'logout Successful',
+        
+    ],200);
+  }
+
+
+
+
+}
--- flowboard-api/routes/api.php
+++ flowboard-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +12,14 @@ Route::get('/ping', function () {
     return response()->json([
         'message' => 'Laravel API Working'
     ]);
+});
+
+
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+
+
+
+Route::group(['middleware'=>'auth:sanctum'],function(){
+Route::post('logout',[AuthController::class,'logout']);
 });
